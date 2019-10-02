7f006688de7218a7ec0ebae5cb7ba0d9ec2ce869
QuachPhuQuoc <Quoc.quach@globee.hk>
2019-10-02 18:29:04 +0700

    Update BaiTapThucHanh

---
+++ BaiTapThucHanh/Chapter2/Chapter2Examples/Chapter2Examples.Android/obj/Debug/90/android/src/md543860ab4c84426c72f0505e776fa2034/MainActivity.java
@@ -0,0 +1,56 @@
+package md543860ab4c84426c72f0505e776fa2034;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"";
+		mono.android.Runtime.register ("Chapter2Examples.Droid.MainActivity, Chapter2Examples.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Chapter2Examples.Droid.MainActivity, Chapter2Examples.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ BaiTapThucHanh/Chapter2/Chapter2Examples/Chapter2Examples/PropertyValuesPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chapter2Examples
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PropertyValuesPage : ContentPage
+    {
+        public PropertyValuesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
