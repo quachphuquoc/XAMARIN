c61bb03f9799992926b91a1b6a54bd337ce51a42
QuachPhuQuoc <Quoc.quach@globee.hk>
2019-10-02 18:22:43 +0700

    update BaiTapLiThuyet

---
+++ BaitapLiThuyet/BaiTap1/BaiTap1/BaiTap1/Login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BaiTap1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login thành công!", "OK");
+        }
+    }
+}
---
+++ BaitapLiThuyet/BaiTap1/BaiTap1/BaiTap1/ManHinhTinhTong.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BaiTap1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ManHinhTinhTong : ContentPage
+    {
+        public ManHinhTinhTong()
+        {
+            InitializeComponent();
+        }
+
+        private void CalSum(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtNum1.Text);
+            double b = double.Parse(txtNum2.Text);
+            txtTong.Text = (a + b).ToString();
+        }
+
+        private void ReturnHomepage(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
